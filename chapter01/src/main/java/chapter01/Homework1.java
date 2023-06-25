a4d01b5197e09df86cddf374c3346764005bb996
rapha <rapha@LAPTOP-7F289603>
2023-06-26 01:00:03 +0900

    happy

---
+++ chapter01/src/main/java/chapter01/Homework1.java
@@ -0,0 +1,18 @@
+package chapter01;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<10 ; i++) {
+			for(int j = 1; j < 10; j++) {
+				System.out.print(j + "x" + i 
+						 + "=" + i*j );
+				System.out.print("\t")	;
+			
+			}System.out.print("\n");
+		}
+	}
+
+}
